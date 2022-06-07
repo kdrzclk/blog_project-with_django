da1b52db4a014721fc8db93ea79c54a2e381d741
kdrzclk <kadir.ozcelik16@gmail.com>
2022-06-07 08:09:05 +0300

    Category model oluşturuldu

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import CharField
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
 
 class Post(models.Model):
 
@@ -13,7 +17,7 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     image = models.ImageField()
-    # category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
     publish_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
