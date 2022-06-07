85e1b137fe5c948e47b74d08f8e36f51aaed258f
kdrzclk <kadir.ozcelik16@gmail.com>
2022-06-07 08:06:44 +0300

    User model çağrıldı

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -15,6 +16,6 @@ class Post(models.Model):
     # category = models.ForeignKey(Category, on_delete=models.PROTECT)
     publish_date = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
-    # author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=10, choices=OPTIONS, default='draft')
     slug = models.SlugField(blank=True, unique=True)
