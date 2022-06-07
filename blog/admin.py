47fe4a4e33ef574509d012a0bdb1552cf1493940
kdrzclk <kadir.ozcelik16@gmail.com>
2022-06-07 11:27:52 +0300

    modeller admin.py dosyasına eklendi

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,11 @@
 from turtle import position
 from django.contrib import admin
-from .models import Category, Post
+from .models import Category, Post, Comment, Like, PostView
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(PostView)
+
