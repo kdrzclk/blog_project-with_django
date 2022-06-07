91e737154c4f1f3ac7f6cddf982450bad69d93c0
kdrzclk <kadir.ozcelik16@gmail.com>
2022-06-07 08:22:31 +0300

    category ve post modellerine __str__ eklendi

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,9 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = 'Categories'
 
+    def __str__(self):
+        return self.name
+
 class Post(models.Model):
 
     OPTIONS = (
@@ -27,5 +30,8 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     status = models.CharField(max_length=10, choices=OPTIONS, default='draft')
     slug = models.SlugField(blank=True, unique=True)
+
+    def __str__(self):
+        return self.title
 
     
