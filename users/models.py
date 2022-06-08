eefe62205d178074118972ac2406080ed5aecd96
kdrzclk <kadir.ozcelik16@gmail.com>
2022-06-09 00:50:26 +0300

    Profile model oluşturuldu

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+def user_profile_path(instance, filename):
+    return 'user/{0}/{1}'.format(instance.user.id, filename)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=user_profile_path, default='avatar.jpg')
+    bio = models.TextField(blank=True)
+
+
