71b4009b3ed7af390366e298f04ed8a60fedd30c
kdrzclk <kadir.ozcelik16@gmail.com>
2022-06-09 01:35:21 +0300

    main urls ayarlandı

--- users/models.py
+++ users/models.py
@@ -10,7 +10,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to=user_profile_path, default='avatar.jpg')
     bio = models.TextField(blank=True)
-
+    
     def __str__(self):
         return "{} {}".format(self.user, 'Profile')
 
