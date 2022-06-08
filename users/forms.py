48d1f9c5b55b7a7c9b7469d0241900782fab1f66
kdrzclk <kadir.ozcelik16@gmail.com>
2022-06-09 02:27:38 +0300

    registration form oluşturuldu

---
+++ users/forms.py
@@ -0,0 +1,17 @@
+from pyexpat import model
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class RegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('username', 'email')
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+
+        if User.objects.filter(email=email).exist():
+            raise forms.ValidationError('Please use another Email, that one already taken!!!')
+        
+        return email
