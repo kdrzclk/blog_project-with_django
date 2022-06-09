9268d6218a3cb4058920f467cd45f37f2c201184
kdrzclk <kadir.ozcelik16@gmail.com>
2022-06-09 19:16:08 +0300

    email check form hazırlandı

--- users/forms.py
+++ users/forms.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, PasswordResetForm
 from django.forms import fields
 from .models import Profile
+from django.contrib import messages
 
 
 class RegistrationForm(UserCreationForm):
@@ -31,3 +32,12 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ("username", "email",  "first_name", "last_name")
+
+class PasswordResetEmailCheck(PasswordResetForm):
+    
+    def clean_email(self):
+        email = self.cleaned_data["email"]
+        if not User.objects.filter(email=email).exists():
+            raise forms.ValidationError("There is no email")
+            
+        return email
--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from .views import profile, register
+from .forms import PasswordResetEmailCheck
 
 urlpatterns = [
     path("register/", register, name="register"),
@@ -11,7 +12,7 @@ urlpatterns = [
 
     path("loginout/", auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
 
-    path("password-reset/", auth_views.PasswordResetView.as_view(template_name='users/password_reset_email.html'), name="password_reset"),
+    path("password-reset/", auth_views.PasswordResetView.as_view(template_name='users/password_reset_email.html', form_class=PasswordResetEmailCheck), name="password_reset"),
 
     path("password-reset/done/", auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name="password_reset_done"),
 
