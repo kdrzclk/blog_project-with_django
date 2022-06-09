44abe5c7f5cba6228233ef8b5ebd8d6eb4773ebe
kdrzclk <kadir.ozcelik16@gmail.com>
2022-06-09 03:39:46 +0300

    profile page hazırlandı

--- users/forms.py
+++ users/forms.py
@@ -1,18 +1,33 @@
-from pyexpat import model
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, PasswordResetForm
+from django.forms import fields
+from .models import Profile
+
 
 class RegistrationForm(UserCreationForm):
-    email = forms.EmailField()
+    email = forms.EmailField()  #override ettik. boş bırakınca default required true oldu
+    
     class Meta:
         model = User
-        fields = ('username', 'email')
-
+        fields = ("username", "email")
+        
     def clean_email(self):
         email = self.cleaned_data['email']
-
-        if User.objects.filter(email=email).exist():
-            raise forms.ValidationError('Please use another Email, that one already taken!!!')
-        
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("Please use another Email, that one already taken")
         return email
+    
+
+        
+class ProfileUpdateForm(forms.ModelForm):
+    
+    class Meta:
+        model = Profile
+        fields = ("image", "bio")
+
+class UserUpdateForm(forms.ModelForm):
+    
+    class Meta:
+        model = User
+        fields = ("username", "email",  "first_name", "last_name")
--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import register
+from .views import profile, register
 
 urlpatterns = [
-    path('register/', register, name='register'),
+    path("register/", register, name="register"),
+    path("profile/", profile, name="profile"),
+
 ]
