c746604eaeb78e471a2f61db7569cfe23cbe4e33
kdrzclk <kadir.ozcelik16@gmail.com>
2022-06-09 16:37:11 +0300

    reset password url template hazırlandı

--- users/urls.py
+++ users/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path("register/", register, name="register"),
     path("profile/", profile, name="profile"),
     path("login/", auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
-    path("loginout/", auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout")
+    path("loginout/", auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout"),
+    path("password-reset/", auth_views.PasswordResetView.as_view(template_name='users/password_reset_email.html'), name="password_reset")
 
 ]
